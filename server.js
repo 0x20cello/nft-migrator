c51449a1f8e3e69d86f163d74d77b74ac8d548d9
Eliseo <felcher.professional@gmail.com>
2022-02-28 21:10:36 +0100

    final

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/<app-name>'));
+app.use(express.static(__dirname + '/dist/web3-nft-migrator'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/<app-name>/index.html'));});
+'/dist/web3-nft-migrator/index.html'));});
 app.listen(process.env.PORT || 8080);
